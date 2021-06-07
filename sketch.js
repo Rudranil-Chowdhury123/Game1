dcd6262314b1b8f1feeef249103c53b9cb16905a
Rudranil-Chowdhury123 <75880270+Rudranil-Chowdhury123@users.noreply.github.com>
2021-06-07 14:49:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var obstacle1, obstacle1Image
+var obstacle2, obstacle2Image
+var obstacle3, obstacle3Image
+var obstacle4, obstacle4Image
+var obstacle5, obstacle5Image
+var obstacle6, obstacle6Image
+var cloud, cloudImage
+var runner, runnerImage
+var out, outImage
+var background, backgroundImage
+var music
+
+function preload(){
+  obstacle1Image = loadImage("obstacle1.png")
+  obstacle2Image = loadImage("obstacle2.png")
+  obstacle3Image = loadImage("obstacle3.png")
+  obstacle4Image = loadImage("obstacle4.png")
+  obstacle5Image = loadImage("obstacle5.png")
+  obstacle6Image = loadImage("obstacle6.png")
+  cloudImage = loadImage("cloud.png")
+  runnerImage = loadImage("man1.png")
+  outImage = loadImage("out.png")
+  backgroundImage = loadImage("background.jpg")
+  music = loadSound("Electric.mp3")
+}
+
+function setup(){
+  
+}
